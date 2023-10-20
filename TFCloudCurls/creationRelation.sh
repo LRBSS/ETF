b2528a85580b15068a661de4a1b0df3ff7f2266c
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-20 14:20:32 +0200

    ALL CURLS

--- TFCloudCurls/creationRelation.sh
+++ TFCloudCurls/creationRelation.sh
@@ -3,5 +3,5 @@ curl \
   --header "Authorization: Bearer GsVrThN1T5vkxA.atlasv1.Fpn9sDlw6BOXsPrVXAx0U4BvBYbLCPzJXTmzUP3yaw4hdf91vcMjaWBZGt5FNqmFTGU" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @team.json \
-  https://app.terraform.io/api/v2/organizations/AlaEssaim/teams
+  --data @relation.json \
+  https://app.terraform.io/api/v2/team-workspaces
