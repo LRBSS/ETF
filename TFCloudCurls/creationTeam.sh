848def5973859c0aa129e4ba5398d90dc9a76b42
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-23 13:39:29 +0200

    Update creationTeam.sh

--- TFCloudCurls/creationTeam.sh
+++ TFCloudCurls/creationTeam.sh
@@ -7,5 +7,5 @@ curl \
   --header "Authorization: Bearer rdutBXOCUC5Cmw.atlasv1.KePN9Tvkrytt77WT97eBPPnd53yZoUpgfJlPqr8yyyoLCnhfQBX1p4p5rGDzIOo9L0g" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST\
-  --data "@team.json" \
+  --data "@./TFCloudCurls/team.json" \
   https://app.terraform.io/api/v2/organizations/AlaEssaim/teams
