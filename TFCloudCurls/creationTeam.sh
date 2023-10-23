fb50a8a836f65901888d3e30dadc389f30f3cbd0
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-23 13:48:03 +0200

    Update creationTeam.sh

--- TFCloudCurls/creationTeam.sh
+++ TFCloudCurls/creationTeam.sh
@@ -4,7 +4,7 @@
 
 # Utilisez `curl` pour effectuer la demande POST avec les données JSON intégrées
 curl \
-  --header "Authorization: Bearer ${{ secrets.TF_API_TOKEN }}" \
+  --header "Authorization: Bearer $1" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST\
   --data "@./TFCloudCurls/team.json" \
