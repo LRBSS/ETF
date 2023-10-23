ad5d23fe786a91bf02cd9836df06e0f61de2db1f
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-23 11:00:44 +0200

    Update creationTeam.sh

--- TFCloudCurls/creationTeam.sh
+++ TFCloudCurls/creationTeam.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
+# Utilisez une Here Document pour inclure les données JSON directement dans le script
+json_data=$(cat <<EOF
+{
+  "data": {
+    "type": "teams",
+    "attributes": {
+      "name": "FirstTeam1",
+      "organization-access": {
+        "manage-workspaces": true
+      }
+    }
+  }
+}
+EOF
+)
+
+# Utilisez `echo` pour afficher les données JSON (à des fins de débogage, si nécessaire)
+echo "$json_data"
+
+# Utilisez `curl` pour effectuer la demande POST avec les données JSON intégrées
 curl \
   --header "Authorization: Bearer 8RFBPtDNVAhKvg.atlasv1.LQL14jwCaxAzLuVIvydoTZeZ8pg2Qi5KtGViwRPZ1m1L4kV65yV0dJPQKpkL6MhSc04" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @team.json \
+  --data "$json_data" \
   https://app.terraform.io/api/v2/organizations/AlaEssaim/teams
