603aaa5540477d06087d6209c4ece5a2e65d0eaa
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-23 10:25:24 +0200

    Update creationTeam.sh

--- TFCloudCurls/creationTeam.sh
+++ TFCloudCurls/creationTeam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl \
-  --header "Authorization: zVn2HpBDItPOjw.atlasv1.O6GsHJjjMq0byozz4VPjx9Qa0O471GjWla0JBCrd0cyTNzS9tZEMZHq23GPpBEQ2mzk
+  --header "Authorization: zVn2HpBDItPOjw.atlasv1.O6GsHJjjMq0byozz4VPjx9Qa0O471GjWla0JBCrd0cyTNzS9tZEMZHq23GPpBEQ2mzk" \
   --request POST \
   --data @./TFCloudCurls/team.json \
   https://app.terraform.io/api/v2/organizations/AlaEssaim/teams
