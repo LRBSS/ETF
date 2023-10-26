6f2692d16225081024840542091123223a06a0b8
LRBSS <130108519+LRBSS@users.noreply.github.com>
2023-10-26 09:24:12 +0200

    Update creationRelation.sh

--- TFCloudCurls/creationRelation.sh
+++ TFCloudCurls/creationRelation.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
+
+json_data=$(cat <<EOF
+{
+  "data": {
+    "attributes": {
+      "access": "custom",
+      "runs": "apply",
+      "variables": "none",
+      "state-versions": "read-outputs",
+      "plan-outputs": "none",
+      "sentinel-mocks": "read",
+      "workspace-locking": false,
+      "run-tasks": false
+    },
+    "relationships": {
+      "workspace": {
+        "data": {
+          "type": "workspaces",
+          "id": "$WORKSPACE_ID"
+        }
+      },
+      "team": {
+        "data": {
+          "type": "teams",
+          "id": "TEAM_ID"
+        }
+      }
+    },
+    "type": "team-workspaces"
+  }
+}
+EOF
+)
+
+
+
+
+
 curl \
-  --header "Authorization: Bearer GsVrThN1T5vkxA.atlasv1.Fpn9sDlw6BOXsPrVXAx0U4BvBYbLCPzJXTmzUP3yaw4hdf91vcMjaWBZGt5FNqmFTGU" \
+  --header "Authorization: Bearer $1" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @relation.json \
+  --data "$json_data"\
   https://app.terraform.io/api/v2/team-workspaces
